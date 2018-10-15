c1f68df23da3f1480a41aafafe83692a1802cd15
RaymondLoranger <Raymond.Loranger@bell.net>
2018-10-15 11:30:18 -0400

    0.1.5

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Hangman.Dictionary.Mixfile do
   def project do
     [
       app: :hangman_dictionary,
-      version: "0.1.4",
+      version: "0.1.5",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps()
