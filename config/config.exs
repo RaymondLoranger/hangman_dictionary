6c0e8931d1d6e3dff1a4144e349920d78a519786
RaymondLoranger <Raymond.Loranger@bell.net>
2020-11-12 14:35:40 -0500

    0.1.14

--- config/config.exs
+++ config/config.exs
@@ -1,7 +1,6 @@
 import Config
 
 # Mix messages in colors...
-config :elixir, ansi_enabled: true
+# config :elixir, ansi_enabled: true
 
-import_config "persist_course_ref.exs"
 import_config "persist_words_path.exs"
